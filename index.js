53015669ad6eec7453cfd9ad6dfa737fb9de2d54
khanhhq2k <khanhhq2k@gmail.com>
2017-08-24 01:44:18 +0700

    add ability to save user record to mongo db

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('./models/User');
+
 require('./services/passport');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
 
+const User = mongoose.model('users');
 passport.use(
   new GoogleStrategy(
     {
@@ -10,9 +12,8 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token:', accessToken);
-      console.log('refresh token:', refreshToken);
-      console.log('profile info: ', profile);
+      // New instance of User model
+      new User({ googleId: profile.id }).save();
     }
   )
 );
