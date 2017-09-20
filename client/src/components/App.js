6cd8eeed41c3102dc3ea3da77b16b694546cbcc4
khanhhq2k <khanhhq2k@gmail.com>
2017-09-21 02:23:48 +0700

    drafting router for basic urls

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Header = () => {
+  return <h2>Header</h2>;
+};
+const Dashboard = () => {
+  return <h2>Dashboard</h2>;
+};
+const SurveyNew = () => {
+  return <h2>SurveyNew</h2>;
+};
+const Landing = () => {
+  return <h2>Landing</h2>;
+};
 
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" component={Landing} />
+            <Route path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
