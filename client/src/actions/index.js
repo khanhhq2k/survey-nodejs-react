10192cea5bec73d3ec87e5e7794d6f275f74f829
khanhhq2k <khanhhq2k@gmail.com>
2017-09-23 17:37:55 +0700

    implement authReducer(in-process)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  // when action creator return a function, THUNK will use dispatch to return
-  // result of function(async)
-  return function(dispatch) {
-    axios
-      .get('/api/current_user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+// when action creator return a function, THUNK will use dispatch to return
+// result of function(async)
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
