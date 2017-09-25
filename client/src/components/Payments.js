d7add0eb74ab7c002262041893a468b34ef6f47f
khanhhq2k <khanhhq2k@gmail.com>
2017-09-25 22:15:00 +0700

    some quick fixes

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,11 +5,15 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name={'Survey'}
+        description={'$5 for 5 credits'}
         amount={500}
         //callback function after receive data from stripe api
         token={token => console.log('Token is: ', token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
