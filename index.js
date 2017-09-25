0b33d980431e234ebfaf9224557c7d6ff949f687
khanhhq2k <khanhhq2k@gmail.com>
2017-09-26 01:46:13 +0700

    install body-parser middleware to get request data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 require('./models/User');
 
@@ -12,6 +13,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in ms
