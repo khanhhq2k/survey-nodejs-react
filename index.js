625af40cd9dc176969d281311d0f827898cd5c9f
khanhhq2k <khanhhq2k@gmail.com>
2017-11-15 03:21:12 +0700

    add new survey model

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 
 require('./models/User');
+require('./models/Survey');
 
 require('./services/passport');
 const keys = require('./config/keys');
